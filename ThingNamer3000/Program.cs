1b741575a47e8f587dc7d7fb56cd3c141a98a169
Ellie <112726692+elliezub@users.noreply.github.com>
2023-09-30 16:18:10 -0400

    add gitignore

---
+++ ThingNamer3000/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind of thing are we talking about here?");
+string itemInput = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string descriptionInput = Console.ReadLine();
+string ofDoom = "of Doom";
+string number = "3000";
+Console.WriteLine($"The {descriptionInput} {itemInput} {ofDoom} {number}!");
