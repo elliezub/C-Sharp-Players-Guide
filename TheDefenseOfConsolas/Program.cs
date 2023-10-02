bf5bfacf50ccec571f72337343c49ca392808d5f
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-02 17:43:31 -0400

    add defense of consolas solution

---
+++ TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Title = "The Defense Of Consolas";
+
+Console.Write("Target row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Deploy to:");
+
+Console.WriteLine($"({row}, {column - 1})");
+Console.WriteLine($"({row - 1}, {column})");
+Console.WriteLine($"({row}, {column + 1})");
+Console.WriteLine($"({row + 1}, {column})");
+
