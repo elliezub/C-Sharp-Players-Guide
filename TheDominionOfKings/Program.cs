9a7860b8d5a8d4cbc89e175b1d6ba5bd093a8e60
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-02 16:08:30 -0400

    Complete dominion of kings problem

---
+++ TheDominionOfKings/Program.cs
@@ -0,0 +1,10 @@
+﻿int calculateKingdomScore(int numberOfEstates, int numberOfDutchies, int numberOfProvinces)
+{
+    int totalEstates = numberOfEstates;
+    int totalDutchies = numberOfDutchies * 3;
+    int totalProvinces = numberOfProvinces * 6;
+
+    return totalEstates + totalDutchies + totalProvinces;
+}
+
+Console.WriteLine(calculateKingdomScore(4, 3, 1));
