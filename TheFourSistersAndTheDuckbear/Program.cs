1ec6e6b6329f1751ff1b0191f05677b876d9327a
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-02 15:51:23 -0400

    solve duckbear problem

---
+++ TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,19 @@
+﻿int remainder;
+int eggsForEachSister;
+
+string remainingEggs(int eggs)
+{
+    remainder = eggs % 4;
+    eggsForEachSister = (eggs - remainder)/4;
+    return $"if there are {eggs} eggs each sister gets {eggsForEachSister} eggs and the duckbear gets {remainder} eggs";
+}
+
+Console.WriteLine(remainingEggs(23));
+Console.WriteLine("Number of total eggs where the duckbear gets more eggs:");
+
+for (int i = 0; i < 20; i++)
+{
+    remainingEggs(i);
+    if (remainder > eggsForEachSister)
+        Console.WriteLine(i);
+}
