0f34e207ddc3dfe7501f99b68bed565456e57371
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-03 12:53:00 -0400

    complete inventory challenge

---
+++ BuyingInventory/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("The following items are available:");
+Console.WriteLine("1 - Rope");
+Console.WriteLine("2 - Torches");
+Console.WriteLine("3 - Climbing Equipment");
+Console.WriteLine("4 - Clean Water");
+Console.WriteLine("5 - Machete");
+Console.WriteLine("6 - Canoe");
+Console.WriteLine("7 - Food Supplies");
+Console.Write("What number do you want to see the price of? ");
+int itemNumber = Convert.ToInt32(Console.ReadLine());
+
+string item = itemNumber switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies",
+    _ => "Invalid Choice"
+};
+
+int price = itemNumber switch
+{
+    1 => 10,
+    2 => 15,
+    3 => 25,
+    4 => 1,
+    5 => 20,
+    6 => 200,
+    7 => 1,
+    _ => 0
+};
+
+Console.WriteLine($"The {item} cost {price} gold");
