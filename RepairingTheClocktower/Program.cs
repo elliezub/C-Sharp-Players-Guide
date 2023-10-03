c514c763b0fe705ac4e384baa5bb34c3d21bd635
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-03 12:27:14 -0400

    complete clock tower challenge

---
+++ RepairingTheClocktower/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter a number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+    Console.WriteLine("Tick");
+else
+    Console.WriteLine("Tock");
+
