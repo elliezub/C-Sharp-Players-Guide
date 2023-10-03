b961fb528055a80eecd1057dc527e80b22ce4513
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-03 14:21:10 -0400

    add magic cannon solution

---
+++ TheMagicCannon/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0  && i % 5 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{i}: Electric and Fire");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Fire");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{i}: Electric");
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.WriteLine($"{i}: Normal");
+    }
+}
