617043eb75adad92f079e11f6e78752c3514f9a8
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-04 16:38:34 -0400

    complete take a number challenge

---
+++ TakingANumber/Program.cs
@@ -0,0 +1,25 @@
+﻿int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+AskForNumber("give me a number!");
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        Console.WriteLine(text);
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < max && number > min)
+            return number;
+        else 
+           Console.WriteLine($"invalid input, enter a number between {min} and {max}");
+    }
+}
+
+AskForNumberInRange("give me a number", 1, 234);
+
