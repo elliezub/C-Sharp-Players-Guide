3a42914522ecd71b3476b4d80ee008e701382f82
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-04 14:32:58 -0400

    edit magic cannon, change background to foreground

--- TheMagicCannon/Program.cs
+++ TheMagicCannon/Program.cs
@@ -1,23 +1,25 @@
 ﻿for (int i = 1; i <= 100; i++)
 {
-    if (i % 3 == 0  && i % 5 == 0)
+    if (i % 3 == 0 && i % 5 == 0)
     {
-        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine($"{i}: Electric and Fire");
     }
     else if (i % 3 == 0)
     {
-        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"{i}: Fire");
     }
     else if (i % 5 == 0)
     {
-        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine($"{i}: Electric");
     }
     else
     {
-        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Black;
         Console.WriteLine($"{i}: Normal");
     }
+    Console.ResetColor(); 
+    Thread.Sleep(100);
 }
