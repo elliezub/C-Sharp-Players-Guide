b4912c227cb80812976150544bd03436b31ff1d8
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-04 16:12:30 -0400

    Solve the laws of french problem

---
+++ TheLawsOfFrench/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentSmallest = int.MaxValue;
+int total = 0;
+
+foreach (int arrayItem in array)
+{
+    if (arrayItem < currentSmallest)
+        currentSmallest = arrayItem;
+
+    total += arrayItem;
+}
+
+float average = (float)total / array.Length;
+
+Console.WriteLine($"smallest: {currentSmallest}");
+Console.WriteLine($"average: {average}");
