231fa8cdeccfc158a154213500b5f490ffe34f2d
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-06 15:34:55 -0400

    complete simulas test

--- SimulasTest/Program.cs
+++ SimulasTest/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ChestState current = ChestState.Locked;
+
+while (true)
+{
+    string currentState = current.ToString().ToLower();
+
+    Console.Write($"The chest is {currentState}, what do you want to do? ");
+
+    string choice = (Console.ReadLine()).ToLower();
+
+    if (current == ChestState.Open && choice == "close") current = ChestState.Closed;
+    if (current == ChestState.Closed && choice == "lock") current = ChestState.Locked;
+    if (current == ChestState.Locked && choice == "unlock") current = ChestState.Unlocked;
+    if (current == ChestState.Unlocked && choice == "open") current = ChestState.Open;
+
+}
+
+enum ChestState { Open, Closed, Locked, Unlocked }
