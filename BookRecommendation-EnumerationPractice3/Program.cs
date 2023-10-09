f306b675955d0459c17854e7874e560d7fd5c07d
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-09 14:55:22 -0400

    added book recommendation logic + switch statement

--- BookRecommendation-EnumerationPractice3/Program.cs
+++ BookRecommendation-EnumerationPractice3/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+do
+{
+    Console.WriteLine("Heres a list of genres:");
+
+    foreach (BookGenre genre in Enum.GetValues(typeof(BookGenre)))
+    {
+        Console.WriteLine($"{(int)genre}. {genre}");
+    }
+
+    Console.Write("Enter a number for a book suggestion: ");
+
+    int input;
+    bool isNumber = int.TryParse( Console.ReadLine(), out input );
+
+    if (isNumber && Enum.IsDefined(typeof(BookGenre), input))
+    {
+        BookGenre currentGenre = (BookGenre)input;
+
+        string result = currentGenre switch
+        {
+            BookGenre.Mystery => "The Alienist",
+            BookGenre.Fantasy => "The Harry Potter series",
+            BookGenre.ScienceFiction => "Journey to the Center of the Earth",
+            BookGenre.SelfHelp => "Atmoic Habits",
+            _ => "no recommendation found"
+        };
+
+        Console.WriteLine(result);
+    }
+    else
+        Console.WriteLine("invalid input");
+
+    Console.WriteLine();
+
+} while (true);
+
+enum BookGenre { Mystery = 1, Fantasy, ScienceFiction, SelfHelp }
