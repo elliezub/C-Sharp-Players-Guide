abb71990fcf7eb046f0f061f0b8f36d3526d9229
Ellie <112726692+elliezub@users.noreply.github.com>
2023-10-09 16:51:40 -0400

    Update Program.cs

--- TuplesPractice/Program.cs
+++ TuplesPractice/Program.cs
@@ -1,3 +1,10 @@
 ﻿(string Name, int WeightInKg, int SpeedInKm) cat = ("Fluffy", 8, 20);
 
 Console.WriteLine($"Name: {cat.Name}, Weight: {cat.WeightInKg}kg, Speed: {cat.SpeedInKm}km/h");
+
+
+// another way to name tuples
+
+var dog = (Name: "Buzz", WeightInKg: "50", SpeedInKm: "30");
+
+Console.WriteLine($"Name: {dog.Name}, Weight: {dog.WeightInKg}kg, Speed: {dog.SpeedInKm}km/h");
